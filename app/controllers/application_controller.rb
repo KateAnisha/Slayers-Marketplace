7781221106448ca4852b56b8416a1809162a7e7c
kate.anisha <kate.anisha@icloud.com>
2021-05-18 14:14:42 +1000

    Setup Rolify, devise and pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    include Pundit
+
+    rescue_from Pundit: NotAuthorizedError, with: :forbidden
+
+    private
+
+    def forbidden
+        flash[:alert] = "You are not authroised to perform that action"
+        redirect_to request.referrer || root_path
+    end
+
 end
